b4ffeb6751362ce053f229f2727e8e8e7f0f592b
v_lyhe <2417847513@qq.com>
2017-07-07 11:55:49 +0800

    update

--- src/test/java/webDriverBrowser/openChrome.java
+++ src/test/java/webDriverBrowser/openChrome.java
@@ -1,6 +1,8 @@
 package webDriverBrowser;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.Test;
@@ -9,9 +11,18 @@ import org.testng.annotations.Test;
  * Created by root on 2017/7/3.
  */
 public class openChrome {
+    WebDriver driver;
     @Test
     public void openChromeTest(){
         System.setProperty("webdriver.chrome.driver","C:\\Users\\root\\IdeaProjects\\ReviewTest1\\drives\\chromedriver.exe");
        WebDriver driver = new ChromeDriver();
+    }
+    @Test
+    public void clickTest(){
+        System.setProperty("webdriver.firefox.bin","D:\\program files\\Mozilla Firefox\\firefox.exe");
+//        启动firefox浏览器
+        driver = new FirefoxDriver();
+        driver.get("http://www.dianzan.it");
+        driver.findElement(By.linkText("社区专题")).click();
     }
 }
